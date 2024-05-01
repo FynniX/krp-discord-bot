f51e56db1bfe3d5f0a9ae041aa5a5e973520e920
Fynn Haupt <47634930+FynniX@users.noreply.github.com>
2024-05-01 18:05:29 +0200

    added check for env variables

---
+++ src/utils/env.ts
@@ -0,0 +1,9 @@
+import 'dotenv/config'
+
+export const check = () => {
+  if (process.env.DATABASE_URL === undefined) throw new Error('DATABASE_URL is not defined')
+  if (process.env.WEBSERVER_URL === undefined) throw new Error('WEBSERVER_URL is not defined')
+  if (process.env.WEBSERVER_PORT === undefined) throw new Error('WEBSERVER_PORT is not defined')
+  if (process.env.MAX_THREADS === undefined) throw new Error('MAX_THREADS is not defined')
+  if (process.env.BOT_TOKEN === undefined) throw new Error('BOT_TOKEN is not defined')
+}
