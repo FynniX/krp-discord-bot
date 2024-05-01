782b5e5f845ac87fef2ef981bda1529e5159785e
Fynn Haupt <47634930+FynniX@users.noreply.github.com>
2024-05-01 23:52:51 +0200

    added env variables

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -6,4 +6,6 @@ export const check = () => {
   if (process.env.WEBSERVER_PORT === undefined) throw new Error('WEBSERVER_PORT is not defined')
   if (process.env.MAX_THREADS === undefined) throw new Error('MAX_THREADS is not defined')
   if (process.env.BOT_TOKEN === undefined) throw new Error('BOT_TOKEN is not defined')
+  if (process.env.GUILD_ID === undefined) throw new Error('GUILD_ID is not defined')
+  if (process.env.PATREON_ROLE === undefined) throw new Error('PATREON_ROLE is not defined')
 }
