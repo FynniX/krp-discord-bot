98db4359527455f90b77104c5a9c5a66123739a2
Fynn Haupt <47634930+FynniX@users.noreply.github.com>
2024-05-01 18:06:06 +0200

    added bot main function

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,28 @@
-console.log('logging from index.ts')
+import { check as env } from './utils/env.js'
+import { app } from './lib/express.js'
+import { client } from './lib/discord.js'
+
+// Check environment variables
+env()
+
+client.on('ready', async () => {
+  console.log('>> Bot started')
+
+  // to create/update/delete discord application commands
+  await client.initApplicationCommands()
+
+  // Start express server
+  app.listen(process.env.PORT, () =>
+    console.log(`>> Webserver started on http://localhost:${process.env.WEBSERVER_PORT}/`)
+  )
+})
+
+client.on('interactionCreate', (interaction) => {
+  client.executeInteraction(interaction)
+})
+
+// import commands
+import './commands/ProfileCommands.js'
+
+// Login discord bot
+client.login(process.env.BOT_TOKEN as string)
