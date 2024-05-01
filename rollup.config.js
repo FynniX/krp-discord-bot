0f046f00f3f3adbc2e06ecb1abf20a02b994adcf
Fynn Haupt <47634930+FynniX@users.noreply.github.com>
2024-05-01 23:51:20 +0200

    added externals to rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,7 @@ import eslint from '@rollup/plugin-eslint'
 
 export default {
   input: 'src/index.ts',
-  external: ['path', 'url', 'dotenv/config', 'express', '@prisma/client', 'discordx', 'discord.js'],
+  external: ['path', 'url', 'dotenv/config', 'express', '@prisma/client', 'discordx', 'discord.js', 'child_process'],
   output: {
     file: 'dist/bundle.js',
     format: 'es'
