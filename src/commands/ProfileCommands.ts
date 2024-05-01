107416c38e02f51eef82e1ad9560f939908a5b80
Fynn Haupt <47634930+FynniX@users.noreply.github.com>
2024-05-01 18:05:42 +0200

    added profile commands

---
+++ src/commands/ProfileCommands.ts
@@ -0,0 +1,102 @@
+import { ApplicationCommandOptionType, CommandInteraction } from 'discord.js'
+import { Discord, Slash, SlashGroup, SlashOption } from 'discordx'
+import { prisma } from '../lib/prisma.js'
+
+@Discord()
+@SlashGroup({ name: 'profile', description: 'Configure your profile' })
+export class ProfileCommands {
+  @SlashGroup('profile')
+  @Slash({ description: 'Set a name for your profile', name: 'name' })
+  async setName(
+    @SlashOption({
+      description: 'Full name for your profile',
+      name: 'name',
+      required: true,
+      type: ApplicationCommandOptionType.String
+    })
+    name: string,
+    interaction: CommandInteraction
+  ) {
+    if (!interaction.member) {
+      interaction.reply({ content: ":x: - Discord member wasn't found", ephemeral: true })
+      return
+    }
+
+    const member = await prisma.member.findUnique({
+      where: { discord: interaction.member.user.id }
+    })
+
+    try {
+      if (!member) {
+        await prisma.member.create({
+          data: {
+            discord: interaction.member.user.id,
+            name
+          }
+        })
+      } else {
+        await prisma.member.update({
+          where: { id: member.id },
+          data: { name }
+        })
+      }
+    } catch (err) {
+      interaction.reply({ content: ':x: - Internal issue with the database', ephemeral: true })
+      return
+    }
+
+    interaction.reply({ content: ':white_check_mark: - Successfully set the name', ephemeral: true })
+  }
+
+  @SlashGroup('profile')
+  @Slash({ description: 'Set a guid for your profile', name: 'guid' })
+  async setGuid(
+    @SlashOption({
+      description: 'Guid for your profile',
+      name: 'guid',
+      required: true,
+      type: ApplicationCommandOptionType.String
+    })
+    guid: string,
+    interaction: CommandInteraction
+  ) {
+    if (!interaction.member) {
+      interaction.reply({ content: ":x: - Discord member wasn't found", ephemeral: true })
+      return
+    }
+
+    const member = await prisma.member.findUnique({
+      where: { discord: interaction.member?.user.id }
+    })
+
+    // Check weather guid is already set
+    if (member?.guid) {
+      interaction.reply({
+        content: ':x: - You can only set the guid once, contact a admin if you want to change it again.',
+        ephemeral: true
+      })
+      return
+    }
+
+    try {
+      if (!member) {
+        await prisma.member.create({
+          data: {
+            discord: interaction.member.user.id,
+            guid
+          }
+        })
+      } else {
+        await prisma.member.update({
+          where: { id: member.id },
+          data: { guid }
+        })
+      }
+    } catch (err) {
+      interaction.reply({ content: ':x: - Internal issue with the database', ephemeral: true })
+      return
+    }
+
+    interaction.reply({ content: ':white_check_mark: - Successfully set the guid', ephemeral: true })
+  }
+}
